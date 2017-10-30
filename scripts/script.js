4ac64cbbbe40817a2a8351b17b039c469d8a9759
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-10-30 18:53:01 +0530

    finally working nicely

--- scripts/script.js
+++ scripts/script.js
@@ -2,19 +2,30 @@
 
        	$('#searchForm').submit(function(e){
        	   e.preventDefault();
-           var searchTerm = $('#search').val();
+           var searchTerm = $('#search').val().toLowerCase();
            var $results = $('#results');
+
            $results.html('Wikipedia results for ' + searchTerm + ' are:' );
            var $url = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=" + searchTerm+ "&limit=10";
+           
            $.getJSON( $url, function(data){
-            var items = JSON.parse(request.responseText);
+            console.log(data);
+            console.log(data[1][0]);
+
+            
+
             var output = '<ul>';
-            for (var key in items){
-              output += '<li>' + items[key].name + '</li>';
-            }
+            $.each(data[1], function(key, val) {
+              output += '<a href="' + data[3][key] + '" target="_blank">';
+              output += '<li>';
+              output += '<h3>' + data[1][key] + '</h3>';
+              output += '<p>'  + data[2][key] + '</p>';
+              output += '</li>';
+              output += '</a>';
+            });
             output += '</ul>';
-            $results.innerHTML = output;
-        	console.log(data);
+            $('#results').html(output);
+        	
         	});
       	});
       
