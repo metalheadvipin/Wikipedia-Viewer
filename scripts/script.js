46081e5e20759903510f925d45d476b9ec7df3f0
metalheadvipin <metalheaddude.vipin@gmail.com>
2017-11-01 17:54:43 +0530

    added loading image

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,13 @@
            var searchTerm = $('#search').val().toLowerCase();
            var $results = $('#results');
 
-           $results.html('Wikipedia results for ' + searchTerm + ' are:' );
+          
            var $url = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=" + searchTerm+ "&limit=10";
            
            $.getJSON( $url, function(data){
             console.log(data);
             console.log(data[1][0]);
-
-            
-
+          
             var output = '<ul>';
             $.each(data[1], function(key, val) {
               output += '<a href="' + data[3][key] + '" target="_blank">';
@@ -22,14 +20,15 @@
               output += '<p>'  + data[2][key] + '</p>';
               output += '</li>';
               output += '</a>';
-            });
+            }); // each loop
+
             output += '</ul>';
             $('#results').html(output);
         	
-        	});
-      	});
+        	}); // getJSON
+      	}); // searchform submit
       
-       });
+       }); // document ready
         
  
   
